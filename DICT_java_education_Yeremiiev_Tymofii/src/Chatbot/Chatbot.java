b1d2a50a7f62d56c350a927a59132e575d7e1744
Tymofii Yeremiiev <t.m.yeremiiev@student.khai.edu>
2023-10-03 15:16:07 +0200

    Stage 2 done

--- DICT_java_education_Yeremiiev_Tymofii/src/Chatbot/Chatbot.java
+++ DICT_java_education_Yeremiiev_Tymofii/src/Chatbot/Chatbot.java
@@ -1,12 +1,17 @@
 package Chatbot;
 
+import java.util.Scanner;
+
 public class Chatbot {
 
-    private String name = "Omega Chat bot++";
-    private String birthYear = "2023";
+    private final String name = "Omega Chat bot++";
+    private final String birthYear = "2023";
+    private final Scanner scan = new Scanner(System.in);
 
     //Main loop
-    private void mainloop(){
+    public void run(){
+        greating();
+        remind_name();
     }
 
     private void greating(){
@@ -15,19 +20,31 @@ public class Chatbot {
     }
 
     private void remind_name(){
-
+        System.out.println("Please, remind me your name");
+        System.out.println("What a great name you have, " + get_input_string());
     }
 
     private String get_input_string(){
-
+        return scan.nextLine();
     }
 
     private void ask_for_age(){
 
     }
 
-    private int get_input_int(){
-
+    private int get_input_int(String text){
+        System.out.println(text);
+        while(true) {
+            if(scan.hasNextInt()){
+                var a = scan.nextInt();
+                scan.nextLine();
+                return a;
+            }
+            else {
+                System.out.println("Incorrect int");
+            }
+            scan.nextLine();
+        }
     }
 
     private void counting(){
