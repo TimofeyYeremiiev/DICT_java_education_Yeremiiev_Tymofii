345db40dde095593e5be458bf40877315cc06742
Tymofii Yeremiiev <t.m.yeremiiev@student.khai.edu>
2023-10-03 15:21:39 +0200

    Stage 3 done

--- DICT_java_education_Yeremiiev_Tymofii/src/Chatbot/Chatbot.java
+++ DICT_java_education_Yeremiiev_Tymofii/src/Chatbot/Chatbot.java
@@ -12,16 +12,17 @@ public class Chatbot {
     public void run(){
         greating();
         remind_name();
+        ask_for_age();
     }
 
     private void greating(){
-        System.out.println("Hello, my name is " + name);
-        System.out.println("I was created in " + birthYear);
+        System.out.println("Hello, my name is " + name + ".");
+        System.out.println("I was created in " + birthYear + ".");
     }
 
     private void remind_name(){
-        System.out.println("Please, remind me your name");
-        System.out.println("What a great name you have, " + get_input_string());
+        System.out.println("Please, remind me your name.");
+        System.out.println("What a great name you have, " + get_input_string() +"!");
     }
 
     private String get_input_string(){
@@ -29,7 +30,13 @@ public class Chatbot {
     }
 
     private void ask_for_age(){
-
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int remainder3 = get_input_int("By 3: ");
+        int remainder5 = get_input_int("By 5: ");
+        int remainder7 = get_input_int("By 7: ");
+        var age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that`s a good time to start programming!");
     }
 
     private int get_input_int(String text){
