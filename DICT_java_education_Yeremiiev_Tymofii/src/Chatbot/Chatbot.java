40d828254eede70aeb7d9e76acb0765aa237b6ef
Tymofii Yeremiiev <t.m.yeremiiev@student.khai.edu>
2023-10-03 15:00:11 +0200

    Stage 1 completed

---
+++ DICT_java_education_Yeremiiev_Tymofii/src/Chatbot/Chatbot.java
@@ -0,0 +1,40 @@
+package Chatbot;
+
+public class Chatbot {
+
+    private String name = "Omega Chat bot++";
+    private String birthYear = "2023";
+
+    //Main loop
+    private void mainloop(){
+    }
+
+    private void greating(){
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I was created in " + birthYear);
+    }
+
+    private void remind_name(){
+
+    }
+
+    private String get_input_string(){
+
+    }
+
+    private void ask_for_age(){
+
+    }
+
+    private int get_input_int(){
+
+    }
+
+    private void counting(){
+
+    }
+
+    private void quitz(){
+
+    }
+}
