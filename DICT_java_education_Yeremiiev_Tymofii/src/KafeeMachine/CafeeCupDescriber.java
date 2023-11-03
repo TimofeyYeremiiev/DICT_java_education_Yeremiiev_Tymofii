cd62ce9bd79dbfa89238b9e9bae857fe81b604bf
Tymofii Yeremiiev <t.m.yeremiiev@student.khai.edu>
2023-11-03 13:09:10 +0100

    Kofeemachine done!

---
+++ DICT_java_education_Yeremiiev_Tymofii/src/KafeeMachine/CafeeCupDescriber.java
@@ -0,0 +1,19 @@
+public class CafeeCupDescriber {
+
+    public String name;
+    public int water;
+    public int milk;
+    public int beans;
+    public int cups;
+    public int price;
+    public CafeeCupDescriber(String name, int water, int milk, int beans, int cups, int price)
+    {
+        this.name = name;
+        this.water = water;
+        this.milk = milk;
+        this.beans = beans;
+        this.cups = cups;
+        this.price = price;
+    }
+
+}
